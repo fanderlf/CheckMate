73844185cc5ea76e498c91e3c18eb2fab97efc02
Florian Fanderl <florian.fanderl@googlemail.com>
2011-08-21 17:14:49 +0200

    Set NaturalSpec up for F# testing

--- Source/NetSolution/CheckMate.Fs.Tests/Module1.fs
+++ Source/NetSolution/CheckMate.Fs.Tests/Module1.fs
@@ -2,3 +2,13 @@
 
 module Module1
 
+open NaturalSpec
+
+let invert x = x + 1
+
+[<Scenario>]
+let ``When the number is 2 it should return 3 - just a test to get NaturalSpec up and running``() =
+    Given 2
+    |> When invert
+    |> It should equal 3
+    |> Verify
