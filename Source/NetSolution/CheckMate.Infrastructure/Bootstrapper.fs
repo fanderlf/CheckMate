d3e2a7bfb468086dc21af61c2aa47356e9c83d90
Florian Fanderl <florian.fanderl@googlemail.com>
2011-08-29 00:16:35 +0200

    Started creating an application Bootstrapper
    Each module can have an IBootstrapperTask
    
    In your main application you will have to reference all
    necessary assemblies as string and pass it to Bootstraper.
    ExecuteBoostrapperTasks(<assemblyName>)
    It will then search for all IBootstrapperTasks in that
    assembly and pass the IContainerBuilder forward so that
    the assembly can register all necessary types.

---
+++ Source/NetSolution/CheckMate.Infrastructure/Bootstrapper.fs
@@ -0,0 +1,16 @@
+﻿namespace CheckMate.Infrastructure.Bootstrapper
+
+open LightCore
+open System
+open System.Reflection
+
+type IBootstrapperTask =
+    abstract member Execute: IContainerBuilder -> unit
+
+type Bootstrapper() =
+    let containerBuilder = new ContainerBuilder()
+    member x.ExecuteBootstrapperTasks assemblyName =
+        let assembly = Assembly.LoadFile assemblyName
+        let tasks = assembly.GetTypes() |> Seq.filter (fun t -> t.GetInterface("IBootstrapperTask") <> null)
+        tasks |> Seq.map (fun t -> Activator.CreateInstance(t) :?> IBootstrapperTask) |> Seq.iter (fun i -> i.Execute(containerBuilder))
+
